dbd5c410825fd3977dea85834d27487d2fc01de7
fire-off <1764947730gky@gmail.com>
2023-04-05 14:39:23 +0800

    测试

--- testing/testpro0/baidudemo.py
+++ testing/testpro0/baidudemo.py
@@ -12,13 +12,16 @@ def test_steps_demo(test_data1):
         driver = webdriver.Edge(r"C:\Users\17649\Downloads\edgedriver_win64\msedgedriver.exe")
         driver.get("https://www.baidu.com")
         driver.maximize_window()
+
     with allure.step(f"输入搜索词：{test_data1}"):
         driver.find_element_by_id("kw").send_keys(test_data1)
         time.sleep(2)
         driver.find_element_by_id("su").send_keys(test_data1)
         time.sleep(2)
+
     with allure.step("保存图片"):
         driver.save_screenshot("./result/b.png")
         allure.attach.file("./result/b.png", attachment_type=allure.attachment_type)
+
     with allure.step("关闭浏览器"):
         driver.quit()
